e0a54a6821be4466844ff8cf7b8d7fd1c10a8240
Evgeny <wwwquadb@gmail.com>
2023-11-10 15:56:32 +0300

    init commit

---
+++ modules/train.py
@@ -0,0 +1,41 @@
+import torch
+import scipy 
+import support_funcs
+
+
+def main():
+    class Model_torch(torch.nn.Module):
+        def __init__():
+            
+            #
+            #
+
+            return 0
+            
+        def forward_prop():
+            
+            #
+            #
+
+            return 0
+
+        def back_prop():
+            
+            #
+            #
+            
+            return 0 
+
+    Train_mod = Model_torch()
+
+
+
+
+    return Train_mod
+
+
+if __name__ == "__main__":
+    model = main()
+    PATH = input("type where to save trained model:\n")
+    torch.save(model.state_dict(), PATH)
+
