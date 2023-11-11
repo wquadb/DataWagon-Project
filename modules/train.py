7b172c755e36ee675f56d6401bbbd7cccbf26083
KekaSick <mlggg@bk.ru>
2023-11-11 19:07:55 +0300

    123

--- modules/train.py
+++ modules/train.py
@@ -1,8 +1,176 @@
 import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+import numpy as np
 import scipy
 import pandas as pd
 import support_funcs
 import support_funcs as sup
+from sklearn.preprocessing import MinMaxScaler
+from copy import deepcopy as dc
+
+
+def prepare_dataframe_for_lstm(df, n_steps):
+    df = dc(df)
+    
+    df.set_index('Date', inplace=True)
+    
+    for i in range(1, n_steps+1):
+        df[f'Close(t-{i})'] = df['Close'].shift(i)
+        
+    df.dropna(inplace=True)
+    
+    return df
+
+
+lookback = 7
+shifted_df = prepare_dataframe_for_lstm(data, lookback) #change data
+shifted_df
+
+
+shifted_df_as_np = shifted_df.to_numpy()
+
+
+scaler = MinMaxScaler(feature_range=(-1, 1))
+shifted_df_as_np = scaler.fit_transform(shifted_df_as_np) #softmax
+
+
+X = shifted_df_as_np[:, 1:]
+y = shifted_df_as_np[:, 0]
+
+
+X = dc(np.flip(X, axis=1))
+
+
+split_index = int(len(X) * 0.95) #idk
+
+
+X_train = X[:split_index]
+X_test = X[split_index:]
+
+y_train = y[:split_index]
+y_test = y[split_index:]
+
+
+X_train = X_train.reshape((-1, lookback, 1))
+X_test = X_test.reshape((-1, lookback, 1))
+
+y_train = y_train.reshape((-1, 1))
+y_test = y_test.reshape((-1, 1))
+
+
+X_train = torch.tensor(X_train).float() # to float
+y_train = torch.tensor(y_train).float()
+X_test = torch.tensor(X_test).float()
+y_test = torch.tensor(y_test).float()
+
+
+class TimeSeriesDataset(Dataset):
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, i):
+        return self.X[i], self.y[i]
+    
+train_dataset = TimeSeriesDataset(X_train, y_train)
+test_dataset = TimeSeriesDataset(X_test, y_test)
+
+
+batch_size = 16
+
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+
+for _, batch in enumerate(train_loader):
+    x_batch, y_batch = batch[0].to(device), batch[1].to(device) #change
+    print(x_batch.shape, y_batch.shape)
+    break
+
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_stacked_layers):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.num_stacked_layers = num_stacked_layers
+
+        self.lstm = nn.LSTM(input_size, hidden_size, num_stacked_layers, 
+                            batch_first=True)
+        
+        self.fc = nn.Linear(hidden_size, 1)
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        h0 = torch.zeros(self.num_stacked_layers, batch_size, self.hidden_size).to(device) #change
+        c0 = torch.zeros(self.num_stacked_layers, batch_size, self.hidden_size).to(device)
+        
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
+
+
+model = LSTM(1, 4, 1) # change parameters
+model.to(device) # change device
+model
+
+
+def train_one_epoch():
+    model.train(True)
+    print(f'Epoch: {epoch + 1}')
+    running_loss = 0.0
+    
+    for batch_index, batch in enumerate(train_loader):
+        x_batch, y_batch = batch[0].to(device), batch[1].to(device)
+        
+        output = model(x_batch)
+        loss = loss_function(output, y_batch)
+        running_loss += loss.item()
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch_index % 100 == 99:  # print every 100 batches
+            avg_loss_across_batches = running_loss / 100
+            print('Batch {0}, Loss: {1:.3f}'.format(batch_index+1,
+                                                    avg_loss_across_batches))
+            running_loss = 0.0
+    print()
+
+
+def validate_one_epoch():
+    model.train(False)
+    running_loss = 0.0
+    
+    for batch_index, batch in enumerate(test_loader):
+        x_batch, y_batch = batch[0].to(device), batch[1].to(device)
+        
+        with torch.no_grad():
+            output = model(x_batch)
+            loss = loss_function(output, y_batch)
+            running_loss += loss.item()
+
+    avg_loss_across_batches = running_loss / len(test_loader)
+    
+    print('Val Loss: {0:.3f}'.format(avg_loss_across_batches))
+    print('***************************************************')
+    print()
+
+
+learning_rate = 0.001
+num_epochs = 10
+loss_function = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) #mb AdamW
+
+for epoch in range(num_epochs):
+    train_one_epoch()
+    validate_one_epoch()
+
 
 if __name__ == "__main__":
     
